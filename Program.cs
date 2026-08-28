97dd1f0b2779b06a9049a856cb74e3ed9f3ece28
m1yazuu <bertschingernils@gmail.com>
2026-08-28 16:56:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World");
+
+Console.Write("Zahl 1: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Zahl 2: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Addition: " + (a + b));
+Console.WriteLine("Subtraktion: " + (a - b));
+Console.WriteLine("Multiplikation: " + (a * b));
+Console.WriteLine("Division: " + (a / b));
+
+Console.ReadLine();
